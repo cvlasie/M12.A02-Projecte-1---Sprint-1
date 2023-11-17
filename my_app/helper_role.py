4aa00b27454ce7c90698529292f6a5698b5870bd
marc <malomi@fp.insjoaquimmir.cat>
2023-11-17 17:00:14 +0100

    4.5 membre 2 (permisos)

--- my_app/helper_role.py
+++ my_app/helper_role.py
@@ -7,6 +7,7 @@ from enum import Enum
 class Role(str, Enum):
     editor = "editor"
     viewer = "viewer"
+    moderator = "moderator"  # Afegir aquesta línia
 
 class Action(str, Enum):
     edit = "create, update and delete"
@@ -15,9 +16,11 @@ class Action(str, Enum):
 # Needs
 __editor_role_need = ActionNeed(Role.editor)
 __viewer_role_need = ActionNeed(Role.viewer)
+__moderator_role_need = ActionNeed(Role.moderator)
 
 __edit_action_need = ActionNeed(Action.edit)
 __view_action_need = ActionNeed(Action.view)
+require_moderator_role = Permission(__moderator_role_need, __edit_action_need, __view_action_need)
 
 # Permissions
 require_editor_role = Permission(__editor_role_need)
@@ -40,6 +43,10 @@ def on_identity_loaded(sender, identity):
             # Role needs
             identity.provides.add(__viewer_role_need)
             # Action needs
+            identity.provides.add(__view_action_need)
+        elif current_user.role == Role.moderator:
+            identity.provides.add(__moderator_role_need)
+            identity.provides.add(__edit_action_need)
             identity.provides.add(__view_action_need)
         else:
             current_app.logger.debug("Unkown role")
--- my_app/routes_admin.py
+++ my_app/routes_admin.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
-from flask_login import current_user, login_required
+from flask_login import login_required
 from .models import User
-from .helper_role import require_editor_role
+from .helper_role import require_editor_role, require_moderator_role  # Assegura't d'importar el nou permís
 from . import db_manager as db
 
 # Blueprint
@@ -11,13 +11,14 @@ admin_bp = Blueprint(
 
 @admin_bp.route('/admin')
 @login_required
-@require_editor_role.require(http_exception=403)
+@require_moderator_role.require(http_exception=403)  # Permetre accés a "admin" i "moderator"
 def admin_index():
-    return render_template('index.html')
+    show_users_button = current_user.role in ['admin', 'moderator']
+    return render_template('index.html', show_users_button=show_users_button)
 
 @admin_bp.route('/admin/users')
 @login_required
-@require_editor_role.require(http_exception=403)
+@require_editor_role.require(http_exception=403)  # Només permetre accés a "admin"
 def admin_users():
     users = db.session.query(User).all()
-    return render_template('users_list.html', users=users)
+    return render_template('users_list.html', users=users, show_users_button=current_user.role == 'admin')
